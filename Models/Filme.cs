362d4fb40b384970f3e04de17b850bcb28bd0332
Cleiton <cleitoncardososp@gmail.com>
2022-06-19 15:55:34 -0300

    Atualizando Modelo Filme

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -8,12 +8,13 @@ namespace Alura.FilmesApi.Models
 {
     public class Filme
     {
-        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo título é obrigatório")]
         public string Titulo { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O campo Diretor é obrigatório"]
         public string Diretor { get; set; }
         public string Genero { get; set; }
-        [Range(1,600)]
+        [Range(1,600, ErrorMessage ="A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
     }
 }
