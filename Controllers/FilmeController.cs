8ad09e307fb31e7cb6cf02af35f60eb72611e2f4
Cleiton <cleitoncardososp@gmail.com>
2022-06-19 15:39:47 -0300

    Criando Entidade Filme

---
+++ Controllers/FilmeController.cs
@@ -0,0 +1,28 @@
+﻿using Alura.FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.FilmesApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+        }
+
+        [HttpGet]
+        public List<Filme> ListaFilmes()
+        {
+            return filmes;
+        }
+    }
+}
