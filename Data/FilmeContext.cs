0fee6d788337e6b73dffaa6e8885baebfd8432d7
Cleiton <cleitoncardososp@gmail.com>
2022-06-20 01:09:29 -0300

    Add pacotes EfCore para conexão MySQL

---
+++ Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using Alura.FilmesApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.FilmesApi.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
