abda50541011f9065d3e329eeaed9dae929c2f95
Cleiton <cleitoncardososp@gmail.com>
2022-07-09 17:11:39 -0300

    update : add classificacao etaria

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -20,6 +20,8 @@ namespace Alura.FilmesApi.Models
         [Range(1,600, ErrorMessage ="A duração deve ter no mínimo 1 e no máximo 600 minutos")]
         public int Duracao { get; set; }
 
+        public int ClassificacaoEtaria { get; set; }
+
         [JsonIgnore]
         public virtual List<Sessao> Sessoes { get; set; }
     }
